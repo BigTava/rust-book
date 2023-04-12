72df3cb8b01169554105758eed35c318dd2e7139
Tiago Tavares <bigtavadev@gmail.com>
2023-04-12 18:04:55 +0100

    feat: learns about common collections

---
+++ 8-collections/src/main.rs
@@ -0,0 +1,80 @@
+use std::collections::HashMap;
+
+fn main() {
+    /* Vectors */
+    let v: Vec<i32> = Vec::new();
+
+    let v = vec![1, 2, 3];
+
+    let mut v = Vec::new();
+
+    v.push(5);
+    v.push(6);
+    v.push(7);
+    v.push(8);
+
+    let v = vec![1, 2, 3, 4, 5];
+
+    let third: &i32 = &v[2];
+    println!("The third element is {third}");
+
+    let third: Option<&i32> = v.get(2);
+    match third {
+        Some(third) => println!("The third element is {third}"),
+        None => println!("There is no third element."),
+    }
+
+    // crashes
+    // let does_not_exist = &v[100];
+
+    // returns none (user friendly, warn user about this)
+    let does_not_exist = v.get(100);
+    
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
+    }
+
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
+
+    /* UTF-8 Encoded Text with Strings */
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let s = format!("{s1}-{s2}-{s3}");
+    // or
+    let s = s1 + "-" + &s2 + "-" + &s3;
+    println!("{}", s);
+
+
+    /* Keys with associated values in hash maps */
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name).copied().unwrap_or(0);
+
+    for (key, value) in &scores {
+        println!("{key}: {value}");
+    }
+
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{:?}", map);
+
+}
