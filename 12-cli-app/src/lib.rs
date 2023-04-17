9dece113ab2d0a14d323d309430848ddc58db37e
Tiago Tavares <bigtavadev@gmail.com>
2023-04-17 13:46:14 +0100

    perf: improves perf with iterators and closures

--- 12-cli-app/src/lib.rs
+++ 12-cli-app/src/lib.rs
@@ -9,17 +9,26 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
 
-        let query: String = args[1].clone();
-        let file_path: String = args[2].clone();
+        let query: String = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let file_path: String = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
 
         let ignore_case: bool = env::var("IGNORE_CASE").is_ok();
 
-        Ok(Config { query, file_path,ignore_case })
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
 }
 
@@ -40,15 +49,10 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results: Vec<&str> = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line: &&str| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(
--- 12-cli-app/src/main.rs
+++ 12-cli-app/src/main.rs
@@ -4,9 +4,7 @@ use std::process;
 use cli::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config: Config = Config::build(&args).unwrap_or_else(|err| {
+    let config: Config = Config::build(env::args()).unwrap_or_else(|err:& str| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
